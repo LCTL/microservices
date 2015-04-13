62bc29bb2d7fb303de4f57b74b041f68382824b1
Lawrence Cheung <lawrence0819@gmail.com>
2015-04-13 11:11:56 +0800

    enhance testFindByUsername

--- user-management-service/user-management-internal-service/src/test/java/com/ctlok/microservices/user/management/test/repository/UserRepositoryTests.java
+++ user-management-service/user-management-internal-service/src/test/java/com/ctlok/microservices/user/management/test/repository/UserRepositoryTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Created by Lawrence Cheung on 2015/4/12.
@@ -39,7 +39,10 @@ public class UserRepositoryTests {
     @Test
     public void testFindByUsername() {
         final User user = userRepository.findByUsername( "Lawrence" );
+        assertThat( "not null", user, notNullValue() );
         assertThat( "username match Lawrence", user.getUsername(), equalTo( "Lawrence" ) );
+        assertThat( "raw password", user.getPassword(), equalTo( "123456" ) );
+        assertThat( "contains ROLE_USER", user.getRoles(), contains( "ROLE_USER" ) );
     }
 
 }
